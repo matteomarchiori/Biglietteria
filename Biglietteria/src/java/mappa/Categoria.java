e7d3c1051f0433ad1c1d23e8c66b15faeedcb9af
dorado3349 <dorado3349@inf1-08.fseveri.it>
2016-04-01 13:33:10 +0200

    Categoria class

--- Biglietteria/src/java/mappa/Categoria.java
+++ Biglietteria/src/java/mappa/Categoria.java
@@ -1,17 +1,60 @@
 package mappa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * Classe di mappa per entità categoria
  * @author matteo
  */
 @Entity
+@Table(name="CATEGORIE")
 public class Categoria {
-    //varibili di esemplare
+    
+//varibili di esemplare
+    
+    @Id @GeneratedValue
+    @Column(name="codice")
     private int codice;
+    
+    @Column(name="descrizione")
     private String descrizione;
+    
+    @Column(name="documento")
     private String documento;
+
+    @Column(name="sconto")
     private int sconto;
+
+    public Categoria() {
+    }//costruttore vuoto
+    
+    public Categoria(String descrizione, String documento, int sconto) {
+        this.descrizione = descrizione;
+        this.documento = documento;
+        this.sconto = sconto;
+    }//costruttore
+
+    public int getCodice() {
+        return codice;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public int getSconto() {
+        return sconto;
+    }
+    
+    
+    
     
 }//Categoria
