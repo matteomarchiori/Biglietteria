9a8624f3249aa93a67feed31e8fe63896b41f89e
matteo <matteo@matteo-Lenovo-G50-80>
2016-04-14 10:22:57 +0200

    Si avvia.

--- Biglietteria/src/java/hibernate/HibernateUtil.java
+++ Biglietteria/src/java/hibernate/HibernateUtil.java
@@ -8,6 +8,8 @@ package hibernate;
 
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
 /**
  * Hibernate Utility class with a convenient method to get Session Factory
@@ -17,18 +19,13 @@ import org.hibernate.SessionFactory;
  */
 public class HibernateUtil {
 
-    private static final SessionFactory sessionFactory;
+    private final static SessionFactory sessionFactory;
     
     static {
-        try {
-            // Create the SessionFactory from standard (hibernate.cfg.xml) 
-            // config file.
-            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
-        } catch (Throwable ex) {
-            // Log the exception. 
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
+        Configuration configuration = new Configuration().configure();
+        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().
+        applySettings(configuration.getProperties());
+        sessionFactory = configuration.buildSessionFactory(builder.build());
     }
     
     public static SessionFactory getSessionFactory() {
